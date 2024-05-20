2cce0db87a2a960c06285886fae65f8fcb1d3f6e
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-05-20 15:24:08 +0200

    book list web component

--- scripts.js
+++ scripts.js
@@ -10,6 +10,120 @@ const data = {
     BOOKS_PER_PAGE
 };
 
+class BookPreview extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        const container = document.createElement('div');
+        container.classList.add('preview');
+
+        const style = document.createElement('style');
+        style.textContent = `
+        .preview {
+            border-width: 0;
+            width: 100%;
+            font-family: Roboto, sans-serif;
+            padding: 0.5rem 1rem;
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            text-align: left;
+            border-radius: 8px;
+            border: 1px solid rgba(var(--color-dark), 0.15);
+            background: rgba(var(--color-light), 1);
+        }
+          
+        @media (min-width: 60rem) {
+        .preview {
+            padding: 1rem;
+        }
+        }
+        
+        .preview_hidden {
+            display: none;
+        }
+        
+        .preview:hover {
+            background: rgba(var(--color-blue), 0.05);
+        }
+        
+        .preview__image {
+            width: 48px;
+            height: 70px;
+            object-fit: cover;
+            background: grey;
+            border-radius: 2px;
+            box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+                0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+        }
+        
+        .preview__info {
+            padding: 1rem;
+        }
+        
+        .preview__title {
+            margin: 0 0 0.5rem;
+            font-weight: bold;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;  
+            overflow: hidden;
+            color: rgba(var(--color-dark), 0.8)
+        }
+        
+        .preview__author {
+            color: rgba(var(--color-dark), 0.4);
+        }
+        `;
+
+        const img = document.createElement('img');
+        img.classList.add('preview__image');
+        img.src = this.getAttribute('image');
+        container.appendChild(img);
+
+        const info = document.createElement('div');
+        info.classList.add('preview__info');
+
+        const title = document.createElement('h3');
+        title.classList.add('preview__title');
+        title.textContent = this.getAttribute('title');
+        info.appendChild(title);
+
+        const author = document.createElement('div');
+        author.classList.add('preview__author');
+        author.textContent = this.getAttribute('author');
+        info.appendChild(author);
+
+        container.appendChild(info);
+        shadow.appendChild(style);
+        shadow.appendChild(container);
+    }
+}
+
+customElements.define('book-preview', BookPreview);
+
+
+class BookList extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        const books = JSON.parse(this.getAttribute('books'));
+        books.forEach(book => {
+            const bookPreview = document.createElement('book-preview');
+            bookPreview.setAttribute('image', book.image);
+            bookPreview.setAttribute('title', book.title);
+            bookPreview.setAttribute('author', book.author);
+            this.shadowRoot.appendChild(bookPreview);
+        });
+    }
+}
+
+customElements.define('book-list', BookList);
+
 
 
 /**
