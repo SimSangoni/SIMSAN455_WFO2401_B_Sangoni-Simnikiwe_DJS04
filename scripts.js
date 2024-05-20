832995f92d61c0e4e99bda22bf820735c41d36b2
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-05-20 19:56:21 +0200

    deleted utils script (not working), everything back to normal now.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 import { initEventListeners } from './eventListeners.js';
-import { renderBookList, setTheme, populateSelectElement } from './utils.js';
-import './BookPreview.js'
+import './BookPreview.js';
 
 initEventListeners();
 
@@ -12,6 +11,65 @@ const data = {
     genres,
     BOOKS_PER_PAGE
 };
+
+/**
+ * Renders a list of book previews from the specified range of books.
+ * @param {Array} books - The array of books to render previews for.
+ * @param {number} start - The index to start rendering from.
+ * @param {number} end - The index to end rendering at.
+ * @returns {DocumentFragment} - The document fragment containing the book previews.
+ */
+
+function renderBookList(books, start, end) {
+    const fragment = document.createDocumentFragment();
+    for (const { author, id, image, title } of books.slice(start, end)) {
+        const bookPreview = document.createElement('book-preview');
+        bookPreview.setAttribute('data-author', data.authors[author]);
+        bookPreview.setAttribute('data-id', id);
+        bookPreview.setAttribute('data-image', image);
+        bookPreview.setAttribute('data-title', title)
+        fragment.appendChild(bookPreview);
+    }
+    return fragment;
+}
+
+
+/**
+ * Sets the theme of the application to either 'day' or 'night'.
+ * @param {string} theme - The theme to set ('day' or 'night').
+ */
+function setTheme(theme) {
+    if (theme === 'night') {
+        document.documentElement.style.setProperty('--color-dark', '255, 255, 255');
+        document.documentElement.style.setProperty('--color-light', '10, 10, 20');
+    } else {
+        document.documentElement.style.setProperty('--color-dark', '10, 10, 20');
+        document.documentElement.style.setProperty('--color-light', '255, 255, 255');
+    }
+}
+
+
+/**
+ * Populates a select element with options from a given object.
+ * @param {HTMLSelectElement} selectElement - The select element to populate.
+ * @param {Object} options - The options object containing key-value pairs.
+ * @param {string} firstOptionText - The text for the first option (default).
+ */
+function populateSelectElement(selectElement, options, firstOptionText) {
+    const fragment = document.createDocumentFragment();
+    const firstOption = document.createElement('option');
+    firstOption.value = 'any';
+    firstOption.innerText = firstOptionText;
+    fragment.appendChild(firstOption);
+    for (const [id, name] of Object.entries(options)) {
+        const option = document.createElement('option');
+        option.value = id;
+        option.innerText = name;
+        fragment.appendChild(option);
+    }
+    selectElement.appendChild(fragment);
+}
+
 
 
 // Initialize variables for page number and filtered matches
