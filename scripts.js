2af41439e95a130e2428d359a1f93705bd274bb6
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-05-20 18:33:42 +0200

    import and initialize eventListeners function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 import './BookPreview.js'
+import { initEventListeners } from './eventListeners.js';
+
+initEventListeners();
+
 // Creating a data object to store books, authors, genres, and the number of books per page
 const data = {
     books,
